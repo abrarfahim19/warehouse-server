198396af4a6de51371fced5dc80d19850736a7ec
abrarfahim19 <fahimpatwari19@gmail.com>
2022-05-08 00:13:41 +0600

    Update product

--- index.js
+++ index.js
@@ -21,8 +21,9 @@ async function run() {
     try {
         await client.connect();
         const productCollection = client.db("wareHouse").collection("product");
+        const userCollection = client.db("wareHouse").collection("user");
         
-        // Get All
+        // Get All (Product)
         app.get("/inventory", async (req, res) => {
             const query = {};
             const cursor = productCollection.find(query);
@@ -30,28 +31,75 @@ async function run() {
             res.send(products);
         });
 
+        
         // Get One
         app.get('/inventory/:id', async (req, res)=>{
             const id = req.params.id;
             const query = {_id: ObjectId(id)};
             const product = await productCollection.findOne(query);
             res.send(product);
-        })
-
+        });
+        
         // POST
         app.post('/inventory', async (req,res)=>{
             const newProduct = req.body;
             const result = await productCollection.insertOne(newProduct);
             res.send(result);
         })
-
+        
         // DELETE
         app.delete('/inventory/:id', async (req,res)=>{
             const id = req.params.id;
             const query = {_id: ObjectId(id)};
             const product = await productCollection.deleteOne(query);
             res.send(product);
+        });
+
+        // Update
+        app.put('/inventory/:id', async (req,res)=>{
+            const id = req.params.id;
+            const filter = {_id: ObjectId(id)};
+            const options = { upsert: true };
+            const {_id,...rest} = req.body; 
+            const updatedProduct = {
+                $set:rest,
+              };
+            const result = await movies.updateOne(filter, updatedProduct, options);
+            console.log(result);
+            res.send(result);
         })
+        
+        // Get All (user)
+        app.get("/user", async (req, res) => {
+            const query = {};
+            const cursor = userCollection.find(query);
+            const users = await cursor.toArray();
+            res.send(users);
+        });
+
+        // Get One (user)
+        app.get('/user/:id', async (req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const product = await userCollection.findOne(query);
+            res.send(product);
+        });
+        
+        // POST (user)
+        app.post('/user', async (req,res)=>{
+            const newProduct = req.body;
+            const result = await userCollection.insertOne(newProduct);
+            res.send(result);
+        })
+        
+        // DELETE (user)
+        app.delete('/user/:id', async (req,res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const product = await userCollection.deleteOne(query);
+            res.send(product);
+        });
+
     } finally {
     }
 }
